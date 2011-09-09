1b1cacef6939f1e7d367f32f7b532334f444654a
Sebastien Rodriguez <sebastien.rodriguez1@gmail.com>
2011-09-09 16:33:05 +0200

    Rajout

---
+++ SecurityArchitecture_and_Design.mm
@@ -0,0 +1,13 @@
+<map version="0.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Security Architecture" ID="ID_1272133856" CREATED="1315484623461" MODIFIED="1315570008137">
+<hook NAME="MapStyle" max_node_width="600"/>
+<node TEXT="what" POSITION="right" ID="ID_512498955" CREATED="1315488664960" MODIFIED="1315488667738"/>
+<node TEXT="why" POSITION="right" ID="ID_1363044470" CREATED="1315488669642" MODIFIED="1315488671000"/>
+<node TEXT="how" POSITION="right" ID="ID_1519595929" CREATED="1315488671375" MODIFIED="1315488673326"/>
+<node TEXT="Security Designs" POSITION="left" ID="ID_1647762916" CREATED="1315488750478" MODIFIED="1315488761841">
+<node TEXT="Zachman Framework" ID="ID_1952353547" CREATED="1315491164887" MODIFIED="1315491173991"/>
+<node TEXT="SABSA: Sherwood Applied Business Security Architecture" ID="ID_731566222" CREATED="1315490964605" MODIFIED="1315490990620"/>
+</node>
+</node>
+</map>
